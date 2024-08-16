f90e6459f838bfb1d16f276a07d310f811e4ef31
[Error_27] <error27dev@gmail.com>
2024-08-16 16:52:29 -0400

    add basic character controller

---
+++ scripts/player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var big_speed: int = 20
+@export var small_speed: int = 5
+
+var is_small := false
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	var h_move := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var v_move := int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	
+	velocity = Vector2(h_move, v_move) * (small_speed if is_small else big_speed)
+	
+	move_and_slide()
