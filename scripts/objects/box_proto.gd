f78b1c3be687d4cfe5d8c0dbf60d510255491e15
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-17 15:23:17 -0400

    added pushable block (incomplete)
    push detection and side being pushed are working

--- scripts/objects/box_proto.gd
+++ scripts/objects/box_proto.gd
@@ -1,5 +1,36 @@
 extends CharacterBody2D
 class_name Box
 
-func push(velocity: Vector2) -> void:
-	pass
+var is_pushed = 0
+var x_direction = 0
+var y_direction = 0 
+
+func _box_physics(delta: float) -> void:
+	# what I'm trying to do: is_pushed == 1 when it is pushed
+	# so if it's not pushed, movement is zero
+	# if it is, take the x & y movements
+	# directions are (up/right) 1 & -1 (down/left)
+	
+	# can be adapted to make blocks that only go in one or a few directions
+	
+	velocity.x += is_pushed*x_direction
+	velocity.y += is_pushed*y_direction
+	move_and_slide()
+
+
+func _on_right_area_body_entered(body: Node2D) -> void:
+	is_pushed = 1
+	print("pushed right side")
+
+
+func _on_right_area_body_exited(body: Node2D) -> void:
+	is_pushed = 0
+
+
+func _on_left_area_body_entered(body: Node2D) -> void:
+	is_pushed = 1
+	print("pushed left side")
+
+
+func _on_left_area_body_exited(body: Node2D) -> void:
+	is_pushed = 0
