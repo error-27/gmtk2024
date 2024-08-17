9e2a46aab4897efa2a9aab21e45942353444c484
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-17 17:14:09 -0400

    block code 2

--- scripts/objects/box_proto.gd
+++ scripts/objects/box_proto.gd
@@ -1,40 +1,56 @@
 extends CharacterBody2D
 class_name Box
 
+var is_push = 0
+var x_direction = 0 
+var y_direction = 0
 
 
-var is_pushed = 0
-var x_direction = 0
-var y_direction = 0 
+func _ready() -> void:
+	velocity.x = 0
+	velocity.y = 0
 
+func _physics_process(delta: float) -> void:
+	#velocity.x = 0
+	#velocity.y = 0
+	velocity.x = is_push*delta*x_direction*16
+	velocity.y = is_push*delta*y_direction*16 
 
-
-func _box_physics(delta: float) -> void:
-	# what I'm trying to do: is_pushed == 1 when it is pushed
-	# so if it's not pushed, movement is zero
-	# if it is, take the x & y movements
-	# directions are (up/right) 1 & -1 (down/left)
-	
-	# can be adapted to make blocks that only go in one or a few directions
-	
-	velocity.x += is_pushed*x_direction
-	velocity.y += is_pushed*y_direction
 	move_and_slide()
+	#print("x: %s" % velocity.x)
+	#print("y: %s" % velocity.y)
+
+
+
+
+
 
 
 func _on_right_area_body_entered(body: Node2D) -> void:
-	is_pushed = 1
-	print("pushed right side")
+	is_push = 1
+	x_direction = 1
 
 
 func _on_right_area_body_exited(body: Node2D) -> void:
-	is_pushed = 0
+	is_push = 0
+	x_direction = 0
 
 
 func _on_left_area_body_entered(body: Node2D) -> void:
-	is_pushed = 1
-	print("pushed left side")
+	is_push = 1
+	x_direction = -1
 
 
 func _on_left_area_body_exited(body: Node2D) -> void:
-	is_pushed = 0
+	is_push = 0
+	x_direction = 0
+
+
+func _on_top_area_body_entered(body: Node2D) -> void:
+	is_push = 1
+	y_direction = -1
+
+
+func _on_top_area_body_exited(body: Node2D) -> void:
+	is_push = 0
+	y_direction = 0
