e2b5217a46d237649efcdc1e460d2cf4213f5893
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-17 18:04:50 -0400

    fixed box code

--- scripts/objects/box_proto.gd
+++ scripts/objects/box_proto.gd
@@ -66,3 +66,13 @@ func _on_top_area_body_entered(body: Node2D) -> void:
 func _on_top_area_body_exited(body: Node2D) -> void:
 	is_push = 0
 	y_direction = 0
+
+
+func _on_bottom_area_body_entered(body: Node2D) -> void:
+	is_push = 1
+	y_direction = 1
+
+
+func _on_bottom_area_body_exited(body: Node2D) -> void:
+	is_push = 0
+	y_direction = 0
