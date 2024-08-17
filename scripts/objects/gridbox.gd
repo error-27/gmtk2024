7c01168e4387c9713a2e3571134cd67d48c0820f
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-17 18:48:17 -0400

    grid box work

--- scripts/objects/gridbox.gd
+++ scripts/objects/gridbox.gd
@@ -1,46 +1,16 @@
 extends CharacterBody2D
-class_name Box
+class_name GridBox
 
 var is_push = 0
 var x_direction = 0 
 var y_direction = 0
-
-#var boxtimer = 0
-
-
-
-func _physics_process(delta: float) -> void:
-	#Basic idea
-	#(is being pushed (0/1) * (time) * (direction to move) * (value)
-	velocity.x = is_push*delta*x_direction*16
-	velocity.y = is_push*delta*y_direction*16 
-
-	move_and_slide()
-
-
-# debug printing
-#	boxtimer += 1
-#	if boxtimer >= (320):
-#		print("-------------------------")
-#		print("x: %s" % velocity.x)
-#		print("y: %s" % velocity.y)
-#		print("is push: %s" % is_push)
-#		print("velocity: %s" % velocity)
-#		print("x direction: %s" % x_direction)
-#		boxtimer -= 320
-
-
-
-
-
-
 
 
 
 
 func _on_right_area_body_entered(body: Node2D) -> void:
 	is_push = 1
-	x_direction = 1
+	position.x -= 64
 
 
 func _on_right_area_body_exited(body: Node2D) -> void:
@@ -60,7 +30,7 @@ func _on_left_area_body_exited(body: Node2D) -> void:
 
 func _on_top_area_body_entered(body: Node2D) -> void:
 	is_push = 1
-	y_direction = 1
+	y_direction = -1
 
 
 func _on_top_area_body_exited(body: Node2D) -> void:
@@ -70,7 +40,7 @@ func _on_top_area_body_exited(body: Node2D) -> void:
 
 func _on_bottom_area_body_entered(body: Node2D) -> void:
 	is_push = 1
-	y_direction = -1
+	y_direction = 1
 
 
 func _on_bottom_area_body_exited(body: Node2D) -> void:
