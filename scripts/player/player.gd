3c6de453d0b831a6356efe9d02aaa8d6ed2d17fc
[Error_27] <error27dev@gmail.com>
2024-08-17 22:05:59 -0400

    shrunklification tiles implemented

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var big_speed: int = 20
 @export var small_speed: int = 5
 
+@export var floor_map: TileMapLayer
+
 var is_small := false
 
 @onready var collider := $CollisionShape2D
@@ -16,6 +18,15 @@ func _process(delta: float) -> void:
 	var v_move := int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	
 	velocity = Vector2(h_move, v_move) * (small_speed if is_small else big_speed)
+	
+	if velocity == Vector2():
+		var cellpos: Vector2i = floor_map.local_to_map(position)
+		var celldata: TileData = floor_map.get_cell_tile_data(cellpos)
+		if celldata != null:
+			if celldata.get_custom_data("Shrunklify"):
+				ensmallify()
+			elif celldata.get_custom_data("Unshrunkle"):
+				enbiggify()
 	
 	move_and_slide()
 
