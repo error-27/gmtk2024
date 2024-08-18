fe3d6b11dcb7a461a9f91c6c3d4a23b47804f5cf
[Error_27] <error27dev@gmail.com>
2024-08-18 17:29:52 -0400

    add floor shrunklification

--- scripts/level_overlord.gd
+++ scripts/level_overlord.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 @export var floor_map: TileMapLayer
 @export var wall_map: TileMapLayer
@@ -20,10 +20,17 @@ func embiggen_level() -> void:
 	objects.hide()
 	var smallify_pos = wall_map.position + Vector2(0, wall_map.get_used_rect().end.y * 16 + 32)
 	player.position = smallify_pos
+	
+	for t in floor_map.get_used_cells():
+		var data = floor_map.get_cell_tile_data(t)
+		if not data == null and not data.get_custom_data("Big Path") == "":
+			var scn = load(data.get_custom_data("Big Path")).instantiate()
+			add_child(scn)
+			scn.position = smallify_pos + Vector2(t.x * 16 * 8, t.y * 16 * 8)
+	
 	for t in wall_map.get_used_cells():
 		var data = wall_map.get_cell_tile_data(t)
 		if not data == null and not data.get_custom_data("Big Path") == "":
-			print(data.get_custom_data("Big Path"))
 			var scn = load(data.get_custom_data("Big Path")).instantiate()
 			add_child(scn)
 			scn.position = smallify_pos + Vector2(t.x * 16 * 8, t.y * 16 * 8)
