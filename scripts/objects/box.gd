7dab73ad8feeeffa4e43cfa46971290abcaf25cd
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-18 10:38:56 -0400

    more box stuff

--- scripts/objects/box.gd
+++ scripts/objects/box.gd
@@ -1,35 +1,37 @@
 extends CharacterBody2D
 class_name Box
 
-var is_push = 0
+var x_push = 0
+var y_push = 0
 var x_direction = 0 
 var y_direction = 0
 
-
 #var boxtimer = 0
+
 
 
 
 func _physics_process(delta: float) -> void:
 	#Basic idea
 	#(is being pushed (0/1) * (time) * (direction to move) * (value)
+ 
 
-	velocity.y = is_push*delta*y_direction*2
-	velocity.x = is_push*delta*x_direction*2
-
+	velocity.y = (y_push - x_push)*(delta*y_direction*2)
+	velocity.x = (x_push- y_push)*(delta*x_direction*2)
 
 	move_and_slide()
 
 
 # debug printing
+
 #	boxtimer += 1
 #	if boxtimer >= (320):
 #		print("-------------------------")
 #		print("x: %s" % velocity.x)
 #		print("y: %s" % velocity.y)
-#		print("side: %s" % side_touch)
 #		print("velocity: %s" % velocity)
-#		print("x direction: %s" % x_direction)
+#		print("x push: %s" % x_push)
+#		print("y push: %s" % y_push)
 #		boxtimer -= 320
 
 
@@ -42,34 +44,35 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_right_area_body_entered(_body: Node2D) -> void:
-	is_push = 1
 	x_direction = 1
-
+	x_push = 1
 
 func _on_right_area_body_exited(_body: Node2D) -> void:
-	is_push = 0
 	x_direction = 0
+	x_push = 0
 
 func _on_left_area_body_entered(_body: Node2D) -> void:
-	is_push = 1
 	x_direction = -1
+	x_push = 1
 
 func _on_left_area_body_exited(_body: Node2D) -> void:
-	is_push = 0
 	x_direction = 0
+	x_push = 0
 
 func _on_top_area_body_entered(_body: Node2D) -> void:
-	is_push = 1
-	y_direction = -1
+	y_direction = 1
+	y_push = 1
+
 
 func _on_top_area_body_exited(_body: Node2D) -> void:
-	is_push = 0
 	y_direction = 0
+	y_push = 0
 
 func _on_bottom_area_body_entered(_body: Node2D) -> void:
-	is_push = 1
-	y_direction = 1
+	y_direction = -1
+	y_push = 1
+
 
 func _on_bottom_area_body_exited(_body: Node2D) -> void:
-	is_push = 0
 	y_direction = 0
+	y_push = 0
