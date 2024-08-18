3c479e8776be37a62cf3ad6a85f1ef5368f5da41
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-18 15:32:04 -0400

    fixed some variables so the box zooms

--- scripts/objects/box.gd
+++ scripts/objects/box.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 class_name Box
 
-var x_push = 0
-var y_push = 0
+var is_push = 0
 var x_direction = 0 
 var y_direction = 0
+@export var x_mult = 1
+@export var y_mult = 512
 
 #var boxtimer = 0
 
@@ -16,8 +17,8 @@ func _physics_process(delta: float) -> void:
 	#(is being pushed (0/1) * (time) * (direction to move) * (value)
  
 
-	velocity.y = (y_push - x_push)*(delta*y_direction*2)
-	velocity.x = (x_push- y_push)*(delta*x_direction*2)
+	velocity.y = (is_push)*(delta*y_direction*y_mult)
+	velocity.x = (is_push)*(delta*x_direction*x_mult)
 
 	move_and_slide()
 
@@ -30,8 +31,8 @@ func _physics_process(delta: float) -> void:
 #		print("x: %s" % velocity.x)
 #		print("y: %s" % velocity.y)
 #		print("velocity: %s" % velocity)
-#		print("x push: %s" % x_push)
-#		print("y push: %s" % y_push)
+#		print("push: %s" % is_push)
+#		print("y direction: %s" % y_direction)
 #		boxtimer -= 320
 
 
@@ -45,34 +46,32 @@ func _physics_process(delta: float) -> void:
 
 func _on_right_area_body_entered(_body: Node2D) -> void:
 	x_direction = 1
-	x_push = 1
+	is_push = 1
 
 func _on_right_area_body_exited(_body: Node2D) -> void:
 	x_direction = 0
-	x_push = 0
-
+	is_push = 0
+	
 func _on_left_area_body_entered(_body: Node2D) -> void:
 	x_direction = -1
-	x_push = 1
-
+	is_push = 1
+	
 func _on_left_area_body_exited(_body: Node2D) -> void:
 	x_direction = 0
-	x_push = 0
-
+	is_push = 0
+	
 func _on_top_area_body_entered(_body: Node2D) -> void:
 	y_direction = 1
-	y_push = 1
-
+	is_push = 1
 
 func _on_top_area_body_exited(_body: Node2D) -> void:
 	y_direction = 0
-	y_push = 0
-
+	is_push = 0
+	
 func _on_bottom_area_body_entered(_body: Node2D) -> void:
 	y_direction = -1
-	y_push = 1
-
-
+	is_push = 1
+	
 func _on_bottom_area_body_exited(_body: Node2D) -> void:
 	y_direction = 0
-	y_push = 0
+	is_push = 0
