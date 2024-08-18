ae899797e5c8ee97dbc0368bc2e2fb9f12b22117
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-17 20:18:30 -0400

    fixed errors

--- scripts/objects/box_proto.gd
+++ scripts/objects/box_proto.gd
@@ -38,41 +38,41 @@ func _physics_process(delta: float) -> void:
 
 
 
-func _on_right_area_body_entered(body: Node2D) -> void:
+func _on_right_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
 	x_direction = 1
 
 
-func _on_right_area_body_exited(body: Node2D) -> void:
+func _on_right_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	x_direction = 0
 
 
-func _on_left_area_body_entered(body: Node2D) -> void:
+func _on_left_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
 	x_direction = -1
 
 
-func _on_left_area_body_exited(body: Node2D) -> void:
+func _on_left_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	x_direction = 0
 
 
-func _on_top_area_body_entered(body: Node2D) -> void:
+func _on_top_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
 	y_direction = 1
 
 
-func _on_top_area_body_exited(body: Node2D) -> void:
+func _on_top_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	y_direction = 0
 
 
-func _on_bottom_area_body_entered(body: Node2D) -> void:
+func _on_bottom_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
 	y_direction = -1
 
 
-func _on_bottom_area_body_exited(body: Node2D) -> void:
+func _on_bottom_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	y_direction = 0
--- scripts/objects/gridbox.gd
+++ scripts/objects/gridbox.gd
@@ -8,41 +8,41 @@ var y_direction = 0
 
 
 
-func _on_right_area_body_entered(body: Node2D) -> void:
+func _on_right_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
-	position.x -= 64
+	x_direction = 1
 
 
-func _on_right_area_body_exited(body: Node2D) -> void:
+func _on_right_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	x_direction = 0
 
 
-func _on_left_area_body_entered(body: Node2D) -> void:
+func _on_left_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
 	x_direction = -1
 
 
-func _on_left_area_body_exited(body: Node2D) -> void:
+func _on_left_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	x_direction = 0
 
 
-func _on_top_area_body_entered(body: Node2D) -> void:
-	is_push = 1
-	y_direction = -1
-
-
-func _on_top_area_body_exited(body: Node2D) -> void:
-	is_push = 0
-	y_direction = 0
-
-
-func _on_bottom_area_body_entered(body: Node2D) -> void:
+func _on_top_area_body_entered(_body: Node2D) -> void:
 	is_push = 1
 	y_direction = 1
 
 
-func _on_bottom_area_body_exited(body: Node2D) -> void:
+func _on_top_area_body_exited(_body: Node2D) -> void:
+	is_push = 0
+	y_direction = 0
+
+
+func _on_bottom_area_body_entered(_body: Node2D) -> void:
+	is_push = 1
+	y_direction = -1
+
+
+func _on_bottom_area_body_exited(_body: Node2D) -> void:
 	is_push = 0
 	y_direction = 0
