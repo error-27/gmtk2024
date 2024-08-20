778ca50fceacc01e686fee79db6485793b7b73bd
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-20 10:33:06 -0400

    world level added level 0

--- scripts/player/world_player.gd
+++ scripts/player/world_player.gd
@@ -124,3 +124,7 @@ func _on_level_2_area_entered(_area: Area2D) -> void:
 
 func _on_level_3_area_entered(_area: Area2D) -> void:
 	get_tree().call_deferred("change_scene_to_file","res://scenes/level/C!'s Levels/level-C-3.tscn")
+
+
+func _on_level_0_area_entered(area: Area2D) -> void:
+	get_tree().call_deferred("change_scene_to_file","res://scenes/level/C!'s Levels/level-W-0.tscn")
