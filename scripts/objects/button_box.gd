fc87e210f83f4b8b9e210e33152e402e454339f1
Wired~Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2024-08-20 08:57:56 -0400

    fixed small error

--- scripts/objects/button_box.gd
+++ scripts/objects/button_box.gd
@@ -32,9 +32,9 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-	var floor: TileMapLayer = get_node("../../LevelOverlord").floor_map
-	if floor.get_cell_atlas_coords(floor.local_to_map(global_position)) == Vector2i(46, 2):
-		button_press.emit(floor.local_to_map(global_position))
+	var box_floor: TileMapLayer = get_node("../../LevelOverlord").floor_map
+	if box_floor.get_cell_atlas_coords(box_floor.local_to_map(global_position)) == Vector2i(46, 2):
+		button_press.emit(box_floor.local_to_map(global_position))
 
 func _on_button_press_area_entered(area: Area2D) -> void:
 	if area.name == "Button_Area":
